c07218d42b54ab411fd333ed60813f70160c3eee
Taylor Panning <tpanning6@gmail.com>
2022-08-24 17:43:24 -0600

    configured aws to work with html template and view upload

--- monolith/api/puppylove_rest/urls.py
+++ monolith/api/puppylove_rest/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import AWSPhotoCreateView, index
 
 urlpatterns = [
     path("", index, name='index'),
+    path("upload/", AWSPhotoCreateView.as_view(), name="photo_form")
     ]
 
--- monolith/api/puppylove_rest/views.py
+++ monolith/api/puppylove_rest/views.py
@@ -3,6 +3,14 @@ from .models import Photo
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+import logging
+from botocore.exceptions import ClientError
+
+
+from .models import AWSPhoto
 
 # Create your views here.
 
@@ -11,3 +19,17 @@ def index(request):
     photos = Photo.objects.all()
     ctx = {'photos': photos}
     return render(request, 'photos/index.html', ctx)
+
+class AWSPhotoCreateView(CreateView):
+    model = AWSPhoto
+    template_name= "photos/upload.html"
+    
+    fields = ['upload', ]
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        photos = AWSPhoto.objects.all()
+        context['Photos'] = photos
+        return context
+
