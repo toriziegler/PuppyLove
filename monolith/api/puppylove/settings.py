ab79bbcb0b463a1d19a0093fdba66a3f44a37d22
nicolas asparria <nasparria@gmail.com>
2022-08-31 17:38:36 -0500

    fixed the django admin

--- monolith/api/puppylove/settings.py
+++ monolith/api/puppylove/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = 'media/'
 
 # Default primary key field type
