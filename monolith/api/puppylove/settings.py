22657f2e0c335ac57130bad0cf30b939b87db776
Taylor Panning <tpanning6@gmail.com>
2022-08-31 16:53:47 -0600

    merge

--- monolith/api/puppylove/settings.py
+++ monolith/api/puppylove/settings.py
@@ -164,8 +164,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = 'media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
