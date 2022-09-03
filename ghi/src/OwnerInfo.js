6b5f35611b8fe0b9a6b9507361ceff4da892cdc7
Victoria Ziegler <toriziegler@cox.net>
2022-09-02 18:13:41 -0700

    fixing poller error wrong cd and fixing ghi build Link imported but not used in ownerinfo

--- ghi/src/OwnerInfo.js
+++ ghi/src/OwnerInfo.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-class OwnerInfo extends React.Component{
-    constructor(props){
+class OwnerInfo extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-            name:'',
-            email:'',
-            phone:'', 
-            description:'',
-            state:'',
-            states:[],
+            name: '',
+            email: '',
+            phone: '',
+            description: '',
+            state: '',
+            states: [],
         };
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -44,17 +43,17 @@ class OwnerInfo extends React.Component{
         };
 
         const response = await fetch(url, fetchConfig);
-        if(response.ok){
+        if (response.ok) {
             this.setState({
-                name:'',
-                email:'',
-                phone:'', 
-                description:'',
+                name: '',
+                email: '',
+                phone: '',
+                description: '',
             });
-        } else if (!response.ok){
+        } else if (!response.ok) {
             const message = ` An error: ${response.status} - ${response.statusText}`;
             throw new Error(message);
-        }            
+        }
 
     }
 
@@ -70,17 +69,17 @@ class OwnerInfo extends React.Component{
         const value = event.target.value;
         this.setState({ phone: value });
     }
-    handleDescriptionChange(event){
+    handleDescriptionChange(event) {
         const value = event.target.value;
         this.setState({ description: value });
     }
-    handleStateChange(event){
+    handleStateChange(event) {
         const value = event.target.value;
         this.setState({ state: value });
     }
 
 
-    
+
     render() {
         // if (this.state.redirect){
         //     return <Navigate to="/login"/>;
@@ -90,65 +89,65 @@ class OwnerInfo extends React.Component{
         // }
         return (
             <div className="App">
-            <div
-              className="container-fluid d-flex align-items-center"
-              style={{
-                height: "100vh",
-                backgroundImage:
-                  "url(https://i.pinimg.com/736x/9d/66/d8/9d66d8de7bcb8fe0da9961df50a95c71--first-kiss-rottweiler-love.jpg)",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat"
-              }}
-            >
-            <div className="signup" id="signuptop">
-                <div className="card mx-auto" style={{ width: "18rem" }}>
-                    <div className="card-body">
-                        <h1>Owner Information</h1>
-                        <hr/>
-                        <form onSubmit={this.handleSubmit} id="create-owner-form">
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleNameChange} value={this.state.name} 
-                            placeholder="Name" required type="text" name="name" 
-                            id="name" />
-                        </div>
+                <div
+                    className="container-fluid d-flex align-items-center"
+                    style={{
+                        height: "100vh",
+                        backgroundImage:
+                            "url(https://i.pinimg.com/736x/9d/66/d8/9d66d8de7bcb8fe0da9961df50a95c71--first-kiss-rottweiler-love.jpg)",
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat"
+                    }}
+                >
+                    <div className="signup" id="signuptop">
+                        <div className="card mx-auto" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <h1>Owner Information</h1>
+                                <hr />
+                                <form onSubmit={this.handleSubmit} id="create-owner-form">
+                                    <div className="form-floating mb-3">
+                                        <input onChange={this.handleNameChange} value={this.state.name}
+                                            placeholder="Name" required type="text" name="name"
+                                            id="name" />
+                                    </div>
 
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleEmailChange} value={this.state.email} 
-                            placeholder="Email" required type="email" name="email" 
-                            id="uemail" />
-                        </div>
-                        
-                        <div className="form-floating mb-3" >
-                            <input  onChange={this.handlePhoneChange} value={this.state.phone}
-                                placeholder="PhoneNumber" required type="text" name="phone" 
-                                id="phone" /> 
-                        </div>
+                                    <div className="form-floating mb-3">
+                                        <input onChange={this.handleEmailChange} value={this.state.email}
+                                            placeholder="Email" required type="email" name="email"
+                                            id="uemail" />
+                                    </div>
 
-                        <div className="form-floating mb-3" >
-                            <input onChange={this.handleDescriptionChange} value={this.state.description}
-                                placeholder="Description" required type="text" name="description" 
-                                id="description"  />
+                                    <div className="form-floating mb-3" >
+                                        <input onChange={this.handlePhoneChange} value={this.state.phone}
+                                            placeholder="PhoneNumber" required type="text" name="phone"
+                                            id="phone" />
+                                    </div>
+
+                                    <div className="form-floating mb-3" >
+                                        <input onChange={this.handleDescriptionChange} value={this.state.description}
+                                            placeholder="Description" required type="text" name="description"
+                                            id="description" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <select onChange={this.handleStateChange} value={this.state.state} required name="state" id="state" className="form-select">
+                                            <option value="">Choose Your State</option>
+                                            {this.state.states.map((state) => {
+                                                return (
+                                                    <option key={state.id} value={state.abbreviation}>
+                                                        {state.name}
+                                                    </option>
+                                                )
+                                            })}
+                                        </select>
+                                    </div>
+                                    <br></br>
+                                    <button className="btn btn-primary" type="submit">Submit</button>
+                                </form>
+                            </div>
                         </div>
-                        <div className="mb-3">
-                    <select onChange={this.handleStateChange} value={this.state.state} required name="state" id="state" className="form-select">
-                        <option value="">Choose Your State</option>
-                            {this.state.states.map((state) => {
-                        return (
-                        <option key={state.id} value={state.abbreviation}>
-                            {state.name} 
-                        </option>
-                            )
-                        })}
-                  </select>
-                </div>
-                        <br></br>
-                        <button className="btn btn-primary" type="submit">Submit</button>
-                        </form>
                     </div>
                 </div>
-            </div>
-            </div>
             </div>
         )
     }
