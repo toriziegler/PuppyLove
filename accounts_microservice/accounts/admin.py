186920b02b00ed1d05d3e4996a823bcb3662b202
nicolas asparria <nasparria@gmail.com>
2022-09-06 19:17:35 -0500

    don't forget to import the .env file

--- accounts_microservice/accounts/admin.py
+++ accounts_microservice/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Owner, State, Note
+
 # Register your models here.
 
 @admin.register(Owner)
--- accounts_microservice/accounts/api_views.py
+++ accounts_microservice/accounts/api_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import AWSPhoto, Owner, State, Note
+from .models import AWSPhoto, Owner, State
 from .encoders import OwnerEncoder, StateEncoder
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -13,7 +13,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import AWSPhoto
 from .serializers import NoteSerializer
