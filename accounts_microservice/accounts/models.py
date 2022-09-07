806f708b69c694990c91efcba9d4a7ea79307275
nicolas asparria <nasparria@gmail.com>
2022-09-07 17:55:56 -0500

    basic merge of auth to main

--- accounts_microservice/accounts/models.py
+++ accounts_microservice/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class State(models.Model):
@@ -36,3 +37,8 @@ class AWSPhoto(models.Model):
         on_delete=models.PROTECT
     )
     upload = models.FileField()
+
+
+# class Note(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+#     body = models.TextField()
