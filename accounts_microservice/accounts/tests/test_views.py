b50d89623f9bdbc427b6fba92ea2622bc468399f
Taylor Panning <tpanning6@gmail.com>
2022-09-13 20:49:40 -0600

    unit test try 1

--- accounts_microservice/accounts/tests/test_views.py
+++ accounts_microservice/accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from accounts.models import Owner
 
 
 class TestStates(TestCase):
@@ -16,8 +17,16 @@ class TestOwners(TestCase):
         self.assertEquals(response.status_code, 200)
 
 
-# class TestOwners(TestCase):
-#     def test_post_owners(self):
-#         client = Client()
-#         response = client.post(reverse("api_owners"))
-#         self.assertEquals(response.status_code, 200)
+class TestPostOwners(TestCase):
+    def test_post_owners(self):
+        testOwner = Owner(
+            name='SecretAgent',
+            email='secret@cia.com',
+            image='None',
+            phone='1234567899',
+            description='Secret',
+            state='Ohio'
+        )
+        client = Client()
+        response = client.post(reverse("api_owners"), testOwner)
+        self.assertEquals(response.status_code, 200)
