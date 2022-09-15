2964ceedd63126116af8ab7baacb59f50deac421
Taylor Panning <tpanning6@gmail.com>
2022-09-15 17:22:46 -0600

    test

--- accounts_microservice/accounts/tests/test_views.py
+++ accounts_microservice/accounts/tests/test_views.py
@@ -23,10 +23,10 @@ class TestOwners(TestCase):
 #         response = client.delete(reverse("api_owners"))
 
 
-class TestUsers(TestCase):
-    def test_list_owners(self):
+class TestPhotos(TestCase):
+    def test_get_photo(self):
         client = Client()
-        response = client.get(reverse("api_users"))
+        response = client.get(reverse("photo_form"))
         self.assertEquals(response.status_code, 200)
 
 # class TestPostOwners(TestCase):
